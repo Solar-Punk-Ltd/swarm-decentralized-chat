2d2c5baf64c42b8fca894e04a842e109e90bbc4d
Peter Ott <ottpeter@tuta.io>
2024-11-26 14:27:48 +0100

    Adds unit tests for readMessagesForAll

--- tests/messages.test.ts
+++ tests/messages.test.ts
@@ -17,3 +17,42 @@ describe('orderMessages (interface for external usage)', () => {
     expect(utilsOrderMessagesSpy).toHaveBeenCalledWith(randomMessages);
   });
 });
+
+
+describe('readMessagesForAll', () => {
+  let chat: SwarmChat;
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+
+    chat = new SwarmChat();
+  });
+
+  it('should return if waitForProcessing is true', () => {
+    const enqueueSpy = jest.spyOn((chat as any).messagesQueue, 'enqueue');
+    const waitForProcessingSpy = jest.spyOn((chat as any).messagesQueue, 'waitForProcessing');
+    waitForProcessingSpy.mockReturnValue(true);
+
+    chat['readMessagesForAll']("example-topic");
+
+    expect(enqueueSpy).not.toHaveBeenCalled();
+  });
+
+  it('should enqueue readMessage if waitForProcessing is false', async () => {
+    const enqueueSpy = jest.spyOn((chat as any).messagesQueue, 'enqueue');
+    const readMessageSpy = jest.spyOn((chat as any), 'readMessage');
+    readMessageSpy.mockImplementation(() => { return null });
+    const users = await userListWithNUsers(2);
+    const waitForProcessingSpy = jest.spyOn((chat as any).messagesQueue, 'waitForProcessing');
+    waitForProcessingSpy.mockResolvedValue(false);
+    chat['users'] = users;
+
+    const readMessagesForAllFn = chat['readMessagesForAll']("example-topic");
+    await readMessagesForAllFn();
+
+    expect(enqueueSpy).toHaveBeenCalledTimes(2);
+    users.forEach(user => 
+      expect(readMessageSpy).toHaveBeenCalledWith(user, "example-topic")
+    );
+  });
+});
